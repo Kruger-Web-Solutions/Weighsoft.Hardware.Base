d208cde4bf4b928d740a416b10f6c33a85a0db21
IRPage <96875276+IRPage@users.noreply.github.com>
2026-05-01 09:01:07 +0200

    feat(serialwriter): add SerialWriterForwarderService header

---
+++ src/examples/serialwriter/SerialWriterForwarderService.h
@@ -0,0 +1,48 @@
+#ifndef SerialWriterForwarderService_h
+#define SerialWriterForwarderService_h
+
+#include <WebSocketsClient.h>
+#include <HTTPClient.h>
+
+#include <HttpEndpoint.h>
+#include <FSPersistence.h>
+#include <SettingValue.h>
+#include <examples/serialwriter/SerialWriterForwarderState.h>
+
+class SerialWriterService;
+
+#define SERIALW_FWD_ENDPOINT_PATH "/rest/serialWriterSource"
+#define SERIALW_FWD_CONFIG_FILE   "/config/serialWriterSource.json"
+
+class SerialWriterForwarderService : public StatefulService<SerialWriterForwarderState> {
+ public:
+  SerialWriterForwarderService(AsyncWebServer* server,
+                               FS* fs,
+                               SecurityManager* securityManager,
+                               SerialWriterService* writerService);
+  void begin();
+  void loop();
+
+  // Called by UartModeService when entering / leaving Writer mode.
+  void start();
+  void stop();
+
+ private:
+  HttpEndpoint<SerialWriterForwarderState>  _httpEndpoint;
+  FSPersistence<SerialWriterForwarderState> _fsPersistence;
+
+  SerialWriterService* _writerService;
+  WebSocketsClient     _wsClient;
+
+  bool          _running = false;
+  unsigned long _nextRetryMs = 0;
+  uint16_t      _backoffMs   = 1000;  // 1s, 2s, 4s, ..., capped at 30s
+
+  void onConfigChanged();
+  void connectWs();
+  void disconnectWs();
+  void onWsEvent(WStype_t type, uint8_t* payload, size_t length);
+  void scheduleRetry();
+};
+
+#endif
